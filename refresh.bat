9cdc5d1193eebcceffec77b4b6b026992a002224
EdgarLiu2 <edgar.liu_cn@139.com>
2018-08-09 00:32:53 +0800

    add mvn docker plugin

--- refresh.bat
+++ refresh.bat
@@ -1,9 +1,15 @@
+REM mklink /j D:\Documents\liuzhao\workspace\appdata\shares\javaweb D:\Documents\liuzhao\workspace\projects\javaweb
 
-set ENV=qa
 set ENV=dev
-
+set ENV=qa
 
 docker-compose -p javaweb_%ENV% -f docker-compose.yml -f docker-compose.%ENV%.yml down
 call mvn package
-docker image build -t javaweb-docker:1.3 .
+REM docker image build -t javaweb-docker:1.3 .
 docker-compose -p javaweb_%ENV% -f docker-compose.yml -f docker-compose.%ENV%.yml up -d
+
+
+REM docker container exec -it javaweb%ENV%_web_1 bash
+REM docker container exec -it javaweb%ENV%_redis-db_1 bash
+REM docker container logs javaweb%ENV%_redis-db_1
+
