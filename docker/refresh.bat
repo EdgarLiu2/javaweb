dde6ab32b9f5609557266c1bb718532bd462a661
EdgarLiu2 <edgar.liu_cn@139.com>
2018-08-11 23:48:28 +0800

    1. move docker files into docker folder 2. setup monitor containers 3. add jenkinins

--- docker/refresh.bat
+++ docker/refresh.bat
@@ -4,16 +4,20 @@ REM REM mklink /j D:\workspace\software\eclipse-jee-photon-R-win32-x86_64\jre D:
 set ENV=dev
 set ENV=qa
 
-docker-compose -p javaweb_%ENV% -f docker-compose.yml -f docker-compose.%ENV%.yml down
+docker-compose -p javaweb_%ENV% -f docker\docker-compose.yml -f docker\docker-compose.%ENV%.yml down
 call mvn package
 REM docker image build -t javaweb-docker:1.3 .
-docker-compose -p javaweb_%ENV% -f docker-compose.yml -f docker-compose.%ENV%.yml up -d
+docker-compose -p javaweb_%ENV% -f docker\docker-compose.yml -f docker\docker-compose.%ENV%.yml up -d
+
+docker-compose -p coreit -f docker\docker-compose.coreit.yml down
+docker-compose -p coreit -f docker\docker-compose.coreit.yml up -d
 
 REM http://192.168.99.100:8080/javaweb
-REM http://192.168.99.100:9080/
-REM http://192.168.99.100:3000/
-REM http://your-server:15672/  id/pwd : sensu/password
+REM http://192.168.99.100:10080
+
+docker container ls
 REM docker container exec -it javaweb%ENV%_web_1 bash
 REM docker container exec -it javaweb%ENV%_redis-db_1 bash
+REM docker container exec -it coreit_jenkins_1 bash
 REM docker container logs javaweb%ENV%_redis-db_1
 REM docker image rm -f $(docker image ls -q)
