069c2a5f72a77950b5452248b73e3206856724d3
EdgarLiu2 <edgar.liu_cn@139.com>
2018-08-11 12:27:44 +0800

    add container monitoring

--- refresh.bat
+++ refresh.bat
@@ -9,7 +9,10 @@ call mvn package
 REM docker image build -t javaweb-docker:1.3 .
 docker-compose -p javaweb_%ENV% -f docker-compose.yml -f docker-compose.%ENV%.yml up -d
 
-
+REM http://192.168.99.100:8080/javaweb
+REM http://192.168.99.100:9080/
+REM http://192.168.99.100:3000/
+REM http://your-server:15672/  id/pwd : sensu/password
 REM docker container exec -it javaweb%ENV%_web_1 bash
 REM docker container exec -it javaweb%ENV%_redis-db_1 bash
 REM docker container logs javaweb%ENV%_redis-db_1
