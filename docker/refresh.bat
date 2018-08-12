f0d0313660fddd7e4e8daaa14f01f4f46224a2dd
liuedg <edgar.liu@msci.com>
2018-08-12 17:50:35 +0800

    deploy to Docker nexus

--- docker/refresh.bat
+++ docker/refresh.bat
@@ -4,20 +4,24 @@ REM REM mklink /j D:\workspace\software\eclipse-jee-photon-R-win32-x86_64\jre D:
 set ENV=dev
 set ENV=qa
 
+cd C:\workspace\eclipse\javaweb
+
+
 docker-compose -p javaweb_%ENV% -f docker\docker-compose.yml -f docker\docker-compose.%ENV%.yml down
 call mvn package
-REM docker image build -t javaweb-docker:1.3 .
 docker-compose -p javaweb_%ENV% -f docker\docker-compose.yml -f docker\docker-compose.%ENV%.yml up -d
 
 docker-compose -p coreit -f docker\docker-compose.coreit.yml down
 docker-compose -p coreit -f docker\docker-compose.coreit.yml up -d
 
 REM http://192.168.99.100:8080/javaweb
-REM http://192.168.99.100:10080
+REM jenkins:	http://192.168.99.100:8082
+REM nexus:		http://192.168.99.100:8081
 
 docker container ls
 REM docker container exec -it javaweb%ENV%_web_1 bash
 REM docker container exec -it javaweb%ENV%_redis-db_1 bash
 REM docker container exec -it coreit_jenkins_1 bash
 REM docker container logs javaweb%ENV%_redis-db_1
+REM docker container logs coreit_nexus_1
 REM docker image rm -f $(docker image ls -q)
