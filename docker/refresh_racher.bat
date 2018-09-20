7daa8d9bc040ec0ab1e16253a658c9f7c39c1c8e
EdgarLiu2 <edgar.liu_cn@139.com>
2018-09-20 16:09:10 +0800

    add tomcat into kubernetes

--- docker/refresh_racher.bat
+++ docker/refresh_racher.bat
@@ -8,12 +8,28 @@ set SHARE=D:\liuzhao\workspace\appdata\shares
 set SHARE=C:\workspace\shares
 
 mkdir %SHARE%\rancher\data\redis
+mkdir %SHARE%\rancher\data\tomcat
 
-kubectl apply -f rancher\javaweb.qa.yaml
-REM kubectl delete -f javaweb.qa.yaml
+IF /I "%ENV%"=="build" (
+	set DOCKER_HOST=tcp://192.168.99.101:2376
+	set DOCKER_MACHINE_NAME=worker1
+	set DOCKER_TLS_VERIFY=1
+	set DOCKER_CERT_PATH=C:\Users\liuedg\.docker\machine\machines\worker1
+	
+	mvn package -f ..\pom.xml
+	docker image ls
+)
+IF /I "%ENV%"=="qa1" (
+	kubectl apply -f rancher\javaweb.redis.qa.yaml -f rancher\javaweb.tomcat.qa.yaml
+	REM kubectl delete -f javaweb.qa.yaml
+)
 
 
+REM kubectl exec -it redis-deployment-5c6d7d78b-z9w4c --namespace=qa1 /bin/bash
+REM kubectl exec -it tomcat-deployment-75b7496d56-7l77d --namespace=qa1 /bin/bash
 REM kubectl exec -it redis-deployment-5569cddfb7-445d6 --namespace=qa1 redis-cli
+REM kubectl logs tomcat-deployment-75b7496d56-7l77d
+
 GOTO EOF
 
 
@@ -22,6 +38,6 @@ echo Usage:
 echo "    refresh_racher.bat qa1"
 
 :EOF
-kubectl get pods --namespace=%ENV%
+kubectl get pods --namespace=%ENV% --output=wide
 kubectl get deployment --namespace=%ENV%
 kubectl get service --namespace=%ENV%
